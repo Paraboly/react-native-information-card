52cf1755d9843642fdebf27443df61be9c280c17
FreakyCoder <kurayogun@gmail.com>
2019-11-08 17:46:58 +0300

    New example and font family is integrated

--- example/App.js
+++ example/App.js
@@ -1,27 +1,26 @@
-import React from "react";
-import { View, StatusBar, SafeAreaView } from "react-native";
-import InformationCard from "@paraboly/react-native-information-card";
+import React from 'react';
+import {View, StatusBar, SafeAreaView} from 'react-native';
+import InformationCard from '@paraboly/react-native-information-card';
 
 const App = () => {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={{flex: 1}}>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView
-        style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
-      >
-        <View style={{ marginTop: 12 }}>
+        style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View style={{marginTop: 12}}>
+          <InformationCard fontFamily="Montserrat-Regular" />
+        </View>
+        <View style={{marginTop: 12}}>
           <InformationCard />
         </View>
-        <View style={{ marginTop: 12 }}>
+        <View style={{marginTop: 12}}>
           <InformationCard />
         </View>
-        <View style={{ marginTop: 12 }}>
+        <View style={{marginTop: 12}}>
           <InformationCard />
         </View>
-        <View style={{ marginTop: 12 }}>
-          <InformationCard />
-        </View>
-        <View style={{ marginTop: 12 }}>
+        <View style={{marginTop: 12}}>
           <InformationCard />
         </View>
       </SafeAreaView>
